f136136280123c98cb9b6affa53acf784922fc2e
Aleyna Eser <aleynaeser345.ma@gmail.com>
2026-01-18 21:33:45 +0300

    🛠️ Fix : Imroves general layout Fixes #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@ import AboutJournal from '@sections/AboutJournal';
 import Journals from '@sections/Journals/Journals';
 import CallForPapers from '@sections/CallForPapers';
 import CurrentArticles from '@sections/CurrentArticles/CurrentArticles';
- 
 
 export default function LandingPage() {
   return (
-    <main className='flex h-full min-h-fit min-w-screen flex-1 flex-col'>
+    <main className='flex h-full min-h-fit flex-1 flex-col'>
       <Slider />
       <AboutJournal />
       <Journals />
